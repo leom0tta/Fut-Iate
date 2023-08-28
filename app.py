3690711e6ba8b854786a0c02e42aebee7024d4d0
Érico Penteado <109689624+ericopent@users.noreply.github.com>
2023-08-28 13:14:56 -0300

    Update app.py

--- app.py
+++ app.py
@@ -65,3 +65,35 @@ sheet_name = 'Ranking'
 url = f'https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}'
 dados = pd.read_csv(url)
 
+st.markdown(
+    "# Fut Iate Rankings :crown: :trophy:"
+)
+
+def head2head(dados, person1, person2, metrica):
+    
+    person1_db = dados[dados['NOME'] == person1]
+    person2_db = dados[dados['NOME'] == person2]
+    
+    if (person2_db[metrica].sum() < person1_db[metrica].sum()):
+        st.markdown(f"**{metrica.title()}: {person1.title()} leva: {person1_db[metrica].sum()} :crown:**")
+
+    elif (person2_db[metrica].sum() > person1_db[metrica].sum()):
+        st.markdown(f"**{metrica.title()}: {person2.title()} leva: {person2_db[metrica].sum()} :crown:**")
+
+    elif (person2_db[metrica].sum() == person1_db[metrica].sum()):
+        st.markdown(f"**{metrica.title()}: EMPATE**")
+
+
+
+with st.expander("# Head-To-Head"):
+    st.markdown("## Head-to-head :rage:")
+    with st.form(key="head2head"):
+        person1 = st.selectbox("Player 1: ", dados['NOME'].unique())
+        person2 = st.selectbox("Player 2: ", [el for el in dados['NOME'].unique() if el != person1])
+        submit_head2head = st.form_submit_button(label="Submit")
+        
+        if(submit_head2head):
+            head2head(dados, person1, person2, 'GOLS')
+            head2head(dados, person1, person2, 'PRESENÇA')
+            head2head(dados, person1, person2, 'ASSISTÊNCIAS')
+            head2head(dados, person1, person2, 'MVP')
