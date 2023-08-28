c43c6361f6b6187aa51f1075be0e35d4bab304d3
Érico Penteado <109689624+ericopent@users.noreply.github.com>
2023-08-28 12:49:30 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,66 @@
+import pandas as pd
+import streamlit as st
+
+
+st.set_page_config(
+    layout="wide",
+    page_title = 'UK - CPI',
+    page_icon='https://gapasset.com.br/favicon.ico')
+    
+st.markdown("""
+    <style>
+    p {
+        font-size:20px;
+        place-items: center;
+
+    }
+    
+    .st-af {
+        font-size: 19px;
+    }
+    
+    .css-10trblm.eqr7zpz0 {
+        
+        font-size:25px;
+    }
+    
+    code {
+        color: rgb(9, 171, 59);
+        overflow-wrap: break-word;
+        font-size: 20px;
+    }   
+    
+    .stMarkdown {
+    display: grid;
+    place-items: center;
+    }
+    
+    button{
+        display: grid;
+        place-items: center;
+        
+    }
+    
+    .stTabs [data-baseweb="tab-list"] button [data-testid="stMarkdownContainer"] p {
+        font-size:24px;
+        text-align: center;
+        place-items: center;
+
+    }
+    
+    .css-3mmywe.e15ugz7a0 {
+        
+        place-items: center;
+        align-content: center;
+        
+    }
+    
+    h2 {text-align: center;}
+    </style>
+    """, unsafe_allow_html=True)
+
+sheet_id = '12_kJDzA7pdejayJ6Hs7tsxJ7ieaUV4-vQ7coBE1BeEM'
+sheet_name = 'Ranking'
+
+url = f'https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}'
+pd.read_csv(url)
